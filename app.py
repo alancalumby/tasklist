ffbebe4a0ade1601f03cb90251f647097e400e46
Alan Calumby <alancalumby@gmail.com>
2021-04-28 23:46:50 -0400

    add table of tasks

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home_page():
-    return render_template('home.html')
+    tasks = [
+        {'id': 1, 'description' : 'Drink water','status': 'InProgress'},
+        {'id': 2, 'description' : 'Eat','status': 'InProgress'},
+        {'id': 3, 'description' : 'Go home','status': 'NotStarted'},
+        {'id': 4, 'description' : 'Study Flask','status': 'Done'}
+    ]
+    return render_template('home.html', tasks = tasks)
 
 @app.route('/about')
 def about_page():
